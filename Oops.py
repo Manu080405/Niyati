b961a1397c6887f7bad776db2472fafea1e3ddb8
Manu <manurejimon12@gmail.com>
2026-02-10 14:32:37 +0530

    Final Push

--- Oops.py
+++ Oops.py
@@ -30,6 +30,7 @@ print(data["user_name"])
 while True:
     print("1.Register")
     print("2.Login")
+    print("3.Exit")
     
     try:
         choice = int(input("Enter the Choice : "))
@@ -37,9 +38,35 @@ while True:
         print("Invalid choice")
         continue
     
-    if choice==1:
-        print("Option not availble")
-        continue
+    if choice == 1:
+        new_user = input("Enter new username : ")
+
+        if new_user in data['user_name']:
+            print("User already exists")
+            continue
+
+        try:
+            new_pass = int(input("Enter new passcode : "))
+        except ValueError:
+            print("Passcode must be numbers only")
+            continue
+
+        try:
+            new_status = int(input("Enter status (1=Active, 0=Inactive) : "))
+        except ValueError:
+            print("Status must be 0 or 1")
+            continue
+
+        data['user_name'].append(new_user)
+        data['passcode'].append(new_pass)
+        data['status'].append(new_status)
+
+        file = open("Detail.csv", "a")
+        file.write(f"\n{new_user},{new_pass},{new_status}")
+        file.close()
+
+        print("Registration Successful")
+
     if choice == 2:
         n = input("Enter the Account name : ")
 
@@ -48,7 +75,6 @@ while True:
             continue
 
         print("User Found")
-
 
         index = data['user_name'].index(n)
 
@@ -62,9 +88,12 @@ while True:
             print("Wrong Passcode")
             continue
 
-
         if data['status'][index] == 1:
             print("Login Successful")
             print("Account is Active ")
         else:
             print("Account is Inactive")
+    
+    if choice==3:
+        print("Exiting......")
+        break
